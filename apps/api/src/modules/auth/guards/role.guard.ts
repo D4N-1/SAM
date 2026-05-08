b538dd2ce94788e75a20b6851931fd38cb347820
DANI <dannyandrestecnologia@outlook.com>
2026-05-08 17:34:11 -0500

    API: Implementación de Decorator y Guard placeholder

---
+++ apps/api/src/modules/auth/guards/role.guard.ts
@@ -0,0 +1,27 @@
+import { CanActivate, ExecutionContext, Injectable } from "@nestjs/common";
+import { Reflector } from "@nestjs/core";
+import { Observable } from "rxjs";
+import { enumRole } from "src/common/enums/role.enum";
+import { ROLES_KEY } from "../decorators/roles.decorator";
+
+
+@Injectable()
+export class RolesGuard implements CanActivate {
+
+    constructor(private reflector: Reflector) {}
+
+
+    canActivate(context: ExecutionContext): boolean | Promise<boolean> | Observable<boolean> {
+        const required = this.reflector.getAllAndOverride<enumRole[]>(
+            ROLES_KEY,
+            [context.getHandler(), context.getClass()]
+        );
+
+        if (!required) return true;
+
+        const request = context.switchToHttp().getRequest();
+        const user = request.user;
+                
+        return required.includes(user.role);
+    }
+}
